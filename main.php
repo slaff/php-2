99cbbe54e44a2d7f665b0fe384dfebfb19f426b3
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-15 11:02:44 +0200

    Advanced for loop with break and continue.

--- main.php
+++ main.php
@@ -1,94 +1,21 @@
 <?php
 // Conditional and looping constructs
-$age = 20;
-$b = "213123213sadfsafsf";
-
-
-$condition1 = $age > 20 && $age < 60; // all parts MUST be true
-$condition2 = $age == 13 || $age == 66;// at least one part MUST be true
-
-$condition3 = $age == 13  || $age == 66 && ($age == 10 || $age > 3 + $b) + null ; // ? 
-            //                  false  && ( false || 
-
-if($condition3) {
-  echo "Yes, it is true";
-}
-
-$ticketPrice = 0;
-
-if($age < 2) {
-  $ticketPrice = 0;
-}
-else if($age > 2 && $age < 16) {
-  $ticketPrice = 30;
-}
-else {
-  $ticketPrice = 60;
-}
-
-
-echo $ticketPrice; // 60
-
-
-// looping constructs
-$rows = [
-    "John",
-    20,
-];
-
-
-echo "\n";
 
 //for
-for($i=0; $i < count($rows); $i++) {
-   echo "$i => ".$rows[$i]."\n"; // 0 => $rows[0]; 0=>John 
+for($i=0; $i < 10; $i++) {
+  // ...
+  if(!($i == 3)) {
+    $i = $i + 2;
+    continue;
+  }  
+
+  if($i == 5) {
+    break;
+  }
+
+
+  echo $i."\n";
 }
 
-exit;
-
-//foreach
-foreach($rows as $key => $value) {
-   echo $key . " = " . $value ."\n";
-}
-
-
-//while
-while($a == 22) {
-  echo "THERE";
-}
-
-//do .. while
-do {
-  echo "HERE"; 
-}
-while($a > 22);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+echo "Finish\n";
 
