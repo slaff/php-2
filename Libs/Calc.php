01b15f64cb35103be468a427fb01538ef4fdb913
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-15 11:17:56 +0200

    Better directory structure.

---
+++ Libs/Calc.php
@@ -0,0 +1,11 @@
+<?php
+
+function plus(float $a, float $b) {
+   return $a + $b;
+}
+
+function minus($a, $b) {
+   return $b - $a;
+}
+
+
--- main.php
+++ main.php
@@ -1,32 +1,4 @@
-<html>
-<body>
 <?php
-// Functions
+require_once __DIR__.'/Libs/Calc.php';
 
-define('MAX_INT', 10);
-
-$d = 10; // GLOBAL variable
-
-function plus(float $a, float $b) {
-   global $d;
-   echo "[$d]".MAX_INT; // constants don't need the global keyword
-
-   $c = 5; // LOCAL variable
-
-   return $a + $b;
-}
-
-function minus($a, $b) {
-   return $b - $a;
-}
-
-plus(1,2);
-?>
-</body>
-
-<?php
-  echo "<!-- comment -->";
-?>
-
-<?= "<!-- comment -->"; ?>
-</html>
+echo plus(1,2);
