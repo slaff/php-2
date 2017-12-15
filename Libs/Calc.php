561dfbc3456b0528670157d80f9d614428921d6d
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-15 12:50:59 +0200

    Added multi method that references another method in the class.

--- Libs/Calc.php
+++ Libs/Calc.php
@@ -17,6 +17,15 @@ class Calc {
         // method accepts two parameters $a, $b
         // in the body you use loop and the internal plus method
 	// to get the final result.
+	public function multi($a,$b) {
+           $result = 0;
+           for ( $i=0; $i<$b; $i++){
+                 $result = $this->plus($result,$a);
+           }
+               
+           $this->memory = $result;
+       	   return $result;
+         }
 
         // method
 	public function minus($a, $b) {
--- main.php
+++ main.php
@@ -4,7 +4,7 @@ require_once __DIR__.'/Libs/SciCalc.php';
 
 use Libs\Calc;
 
-
 $calc = new Calc();
 $calc->minus(1, 3);
-echo $calc->getMemory()."\n"; // ERROR
+echo $calc->multi(3,4);
+// echo $calc->getMemory()."\n"; // ERROR
