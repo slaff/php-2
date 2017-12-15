fb126117255eada62e5652cfddf4a230d2fe088c
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-15 09:20:49 +0200

    Example of comments.

---
+++ main.php
@@ -0,0 +1,25 @@
+<?php
+
+// single line comment.
+
+/*
+
+This is 
+multi 
+line comment
+
+*/
+
+
+/**
+  Document Comment
+  this is similar to multi-line comment
+  but is used to add documentation
+*/
+
+
+echo "Hello World";
+
+
+
+?>
