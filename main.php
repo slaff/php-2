659751ee19177a2e34394fdb57a870f82a08d995
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-15 10:06:59 +0200

    Added type casting and type juggling.

--- main.php
+++ main.php
@@ -1,67 +1,23 @@
 <?php
+// Type casting and ...
+$a = "123a5sasas11";
+$b = 20.1;
 
-// [_a-zA-Z][0-9a-zA-Z_]*
+// var_dump((int)$a);
 
-// Data types 
+// juggling
 
-// (strings, integers, floats, booleans, arrays, resources, and null)
+$c = $a + $b;
 
-$lastName = "Smith";
-echo 'John $lastName'."\n"; 
-echo "John $lastName\n";
+// var_dump($c);
 
-// integers
-$age = 20;
+// Concatenation
 
-// floats
-$age = 20.5;
+$d = $a . $b; // 
 
-// booleans
-$success = true; // false
+// echo $d;
 
-// arrays
-$rows = [ 3, 6, 9 ];
-echo count($rows); // number of elements: 3
-echo $rows[0]; // 3
-echo $rows[2]; // 9
-echo $rows[0] + $rows[1]; // 9
-
-$rows = [
-  "name" => "John",
-   4 => 12,
-  "lastName" => "Smith",
-  "age" => 20,
-   5=> 13,
-   6=> 14,
-];
-
-echo $rows["name"]; // John
-echo $rows["age"] . $rows[4]; // "2012"
-
-$rows = [
-  0 => [ "name"=>"John", "age"=> [12, 342] ],
-  1 => [ "name"=>"Peter", "age"=> [22, 33] ],
-  2 => [ "name"=>"Adam", "age"=> [1, 6] ],
-];
-
-echo $rows[2]["age"]; // 42
-
-echo $rows[2]["age"][1] . $rows[1]["age"][0]; // 6 and 22
-
-// and null
-
-$var; // == $var = null
-
-echo "[one". null. "two]"; // [onetwo]
-echo 2 + null; // 2+0 = 2
-
-// resources
-$fp = fopen("/tmp/data.txt","r");
-fread($fp, 200);
-//...
-fclose($fp);
-
-
+echo ($a + $b)/($a.$b); // ? (143.1) / ("123a5sasas1120.1") = 143.1 / 123
 
 
 ?>
