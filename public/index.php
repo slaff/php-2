fdebaceb953ab09b3444de9826f72d5abcaa877a
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-19 12:23:57 +0200

    JSON related task.

--- public/index.php
+++ public/index.php
@@ -7,34 +7,16 @@ use Libs\{Db\Customer, Db\Repository};
 
 $config = include_once __DIR__.'/../config/application.config.php';
 
-// read an XML file
+// Read the XML
+$data = file_get_contents(__DIR__.'/../composer.json');
 
-$xml = simplexml_load_file( __DIR__.'/../data/produce.xml' );
+$json = json_decode($data, true);
 
-/*
-<?xml version = "1.0" encoding = "UTF-8" ?>
-<produce xmlns:ea = "test">
-    <vegetables>
-        <vegetable unit = "pound">
-            <name> tomatoes </name>
-            <price season="high"> 2.99 </price>
-            <price season="low"> 2.99 </price>
-        </vegetable>
-        <vegetable unit = "pound">
-            <name> olives </name>
-            <price season="high"> 1.99 </price>
-            <price season="low"> 1.22 </price>
-        </vegetable>
-    </vegetables>
-</produce>
-*/
+// TODO: 1. Change "src\/Libs\/" => "src\/"
+//       2. Save/Display the array again as JSON
 
-// get the price of Tomatos in High season
-$xml->vegetables->vegetable[0]->price[0] = 4.55;
+// $json["require"]["swiftmailer/swiftmailer"] = "^132323";
 
-echo $xml->asXML();
-
-
-
+echo json_encode($json);
 
 
