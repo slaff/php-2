c95a6d890c5dddb369975530ee59b650bdd07c67
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-19 11:51:12 +0200

    Added new XML task.

--- public/index.php
+++ public/index.php
@@ -7,25 +7,27 @@ use Libs\{Db\Customer, Db\Repository};
 
 $config = include_once __DIR__.'/../config/application.config.php';
 
-$customer = new Customer();
+// read an XML file
 
-echo "<html><body>";
-echo "<pre>";
+$xml = simplexml_load_file( __DIR__.'/../data/produce.xml' );
 
-//$pattern = '/^<A\s+href="(.*?)"><\/a>$/i';
-//$pattern = '/ href="(.*?)"[ |>]/i';
+/*
+<?xml version = "1.0" encoding = "UTF-8" ?>
+<produce xmlns:ea = "test">
+    <vegetables>
+        <vegetable unit = "pound">
+            <name> tomatoes </name>
+            <price season="high"> 2.99 </price>
+            <price season="low"> 2.99 </price>
+        </vegetable>
+        <vegetable unit = "pound">
+            <name> olives </name>
+            <price season="high"> 1.99 </price>
+            <price season="low"> 1.22 </price>
+        </vegetable>
+    </vegetables>
+</produce>
+*/
 
-$pattern = '/"(.*?)"/'; //Christina’s idea, Nikola’s implementation
-
-$text = '<a href="URL"></a>'; // TODO: match the URL and echo it
-
-if(preg_match($pattern, $text, $matches)) {
-   echo "Valid Email Address";
-   var_dump($matches);
-}
-else {
-   echo "Not Valid Email Address";
-}
-
-echo "</pre>";
-echo "</body></html>";
+// TODO: get the price of Tomatos in High season
+echo $xml->vegetables->vegetable[1]->price[1]."\n";
