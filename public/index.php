513f9ca43bcc846c96349fb85bd949f5a56bb1ce
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-19 09:20:58 +0200

    Added regex to match a valid variable name.

--- public/index.php
+++ public/index.php
@@ -7,21 +7,19 @@ use Libs\{Db\Customer, Db\Repository};
 
 $config = include_once __DIR__.'/../config/application.config.php';
 
-// Get from DB a customer with id 4
-$repository = new Repository($config['db']);
-$customer = $repository->findById(4);
-if($customer == null) {
-  $customer = new Customer();
+echo "<html><body>";
+echo "<pre>";
+
+$pattern = "/^\\$([a-zA-Z_][a-zA-Z0-9_]*)$/"; // 
+$text = '$abc1';
+
+if(preg_match($pattern, $text, $matches)) {
+   echo "Valid Variable";
+   var_dump($matches);
+}
+else {
+   echo "Not Valid Variable";
 }
 
-header("Content-Type: text/html; charset=utf-16");
-
-
-echo $customer->firstname."\n";
-
-// We work with the entity object
-
-$customer->lastName = "Spirou";
-
-// TODO: Store the data in the database
-$repository->persist($customer);
+echo "</pre>";
+echo "</body></html>";
