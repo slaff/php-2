883aef60c6816a85c93c066c47d98967160e6148
Slavey Karadzhov <slavey.karadzhov@roguewave.com>
2017-12-19 09:34:41 +0200

    Magic Method matching.

--- public/index.php
+++ public/index.php
@@ -10,8 +10,8 @@ $config = include_once __DIR__.'/../config/application.config.php';
 echo "<html><body>";
 echo "<pre>";
 
-$pattern = "/^\\$([a-zA-Z_]\w{0,})$/"; //
-$text = '$a';
+$pattern = '/^\$_{2}(\w{1,})\(\)$/'; // Magic Method Matching
+$text = '$__something()';
 
 if(preg_match($pattern, $text, $matches)) {
    echo "Valid Variable";
